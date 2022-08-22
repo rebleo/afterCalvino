005b4dafea6710eff4d99c9387fc62e414e0ca79
rebecca (marks) leopold <rebleo@rebeccas-MacBook-Pro.local>
2022-08-22 11:13:03 -0400

    init Calvino

---
+++ invisibleCities.js
@@ -0,0 +1,3 @@
+console.log("Kublai Khan does not necessarily believe everything Marco Polo says when he describes the cities visited on his expeditions, but hte meperor of the Tartars does continue listening to the young Venetian with greater attentino and curiosity  than he shoes any other messenger or explorer of his.")
+
+
